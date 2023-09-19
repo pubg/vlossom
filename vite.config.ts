8faa9c8cf22638c356ec7401d0034842d44fe08c
gmldus <gmldus0407@naver.com>
2023-09-19 10:54:10 +0900

    Prepare publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/main.ts'),
+            name: 'Vlossom',
+            fileName: 'vlossom',
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+    },
+});
