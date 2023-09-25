1554a54f255fd8280c16b7da03fd792d609b9add
Hee Yeon Hwang <gmldus0407@naver.com>
2023-09-26 01:07:22 +0900

    docs: create project (#1)

---
+++ packages/docs/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Vlossom Document</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        return {};
+    },
+});
+</script>
