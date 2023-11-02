14e00d952c286d9f3a3c87c58c193fd82042c027
gmldus <gmldus0407@naver.com>
2023-11-02 10:34:37 +0900

    feat(style): enable to test dark mode

--- packages/vlossom/.storybook/preview.ts
+++ packages/vlossom/.storybook/preview.ts
@@ -2,6 +2,27 @@ import type { Preview } from '@storybook/vue3';
 
 import '@/styles/index.scss';
 
+const decorators = [
+    (story, context) => {
+        const backgrounds = context.globals.backgrounds;
+
+        if (backgrounds) {
+            if (context.globals.backgrounds.value === '#f8f8f8') {
+                document.body.classList.remove('vs-dark');
+                document.body.classList.add('vs-light');
+            } else {
+                document.body.classList.remove('vs-light');
+                document.body.classList.add('vs-dark');
+            }
+        }
+
+        return {
+            components: { story },
+            template: '<story />',
+        };
+    },
+];
+
 const preview: Preview = {
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,7 +32,21 @@ const preview: Preview = {
                 date: /Date$/,
             },
         },
+        backgrounds: {
+            default: 'light',
+            values: [
+                {
+                    name: 'light',
+                    value: '#f8f8f8',
+                },
+                {
+                    name: 'dark',
+                    value: '#24252a',
+                },
+            ],
+        },
     },
+    decorators,
 };
 
 export default preview;
