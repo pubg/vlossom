2453de389ffbe149ad182ec760be1ee7a4aba722
smithoo <smithoo@pubg.com>
2023-11-20 12:53:03 +0900

    Update name input name

---
+++ packages/vlossom/src/components/vs-name-input/VsNameInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <input v-model="value" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const value = ref('');
+
+        return {
+            value,
+        };
+    },
+});
+</script>
