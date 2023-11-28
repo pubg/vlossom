8b8ebb9d2de9245dab4ff5f2912bfcd59555bfde
gmldus <gmldus0407@naver.com>
2023-11-28 12:50:18 +0900

    fix test

--- packages/vlossom/src/composables/__tests__/responsive-composable.test.ts
+++ packages/vlossom/src/composables/__tests__/responsive-composable.test.ts
@@ -3,7 +3,7 @@ import { useResponsiveWidth } from '../responsive-composable';
 import { ref } from 'vue';
 
 describe('useResponsiveWidth composable', () => {
-    it('width', () => {
+    it('width with all breakpoints', () => {
         const { widthVariables, widthClasses } = useResponsiveWidth(
             ref({ xl: '100px', lg: '150px', md: '200px', sm: '250px', xs: '300px' }),
             ref({}),
@@ -19,7 +19,17 @@ describe('useResponsiveWidth composable', () => {
         expect(widthClasses.value).toEqual(['vs-width-xs', 'vs-width-sm', 'vs-width-md', 'vs-width-lg', 'vs-width-xl']);
     });
 
-    it('grid', () => {
+    it('width with some breakpoints', () => {
+        const { widthVariables, widthClasses } = useResponsiveWidth(ref({ lg: '20%', sm: '50%' }), ref({}));
+
+        expect(widthVariables.value).toEqual({
+            '--vs-width-sm': '50%',
+            '--vs-width-lg': '20%',
+        });
+        expect(widthClasses.value).toEqual(['vs-width-sm', 'vs-width-lg']);
+    });
+
+    it('grid with all breakpoints', () => {
         const { widthVariables, widthClasses } = useResponsiveWidth(
             ref({}),
             ref({ xl: 1, lg: 2, md: 3, sm: 4, xs: 6 }),
@@ -33,5 +43,15 @@ describe('useResponsiveWidth composable', () => {
             '--vs-width-xl': 'calc(1/12 * 100%)',
         });
         expect(widthClasses.value).toEqual(['vs-width-xs', 'vs-width-sm', 'vs-width-md', 'vs-width-lg', 'vs-width-xl']);
+    });
+
+    it('grid with some breakpoints', () => {
+        const { widthVariables, widthClasses } = useResponsiveWidth(ref({}), ref({ lg: 4, md: 6 }));
+
+        expect(widthVariables.value).toEqual({
+            '--vs-width-md': 'calc(6/12 * 100%)',
+            '--vs-width-lg': 'calc(4/12 * 100%)',
+        });
+        expect(widthClasses.value).toEqual(['vs-width-md', 'vs-width-lg']);
     });
 });
