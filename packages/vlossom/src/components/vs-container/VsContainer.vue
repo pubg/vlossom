6ce2df0702032135b34a8796dd64629c731e74ae
Hee Yeon Hwang <gmldus0407@naver.com>
2023-11-30 11:12:32 +0900

    feat(VsContainer, VsWrapper): add component (#17)

---
+++ packages/vlossom/src/components/vs-container/VsContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="vs-container">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const VsContainer = defineComponent({
+    name: 'vs-container',
+    setup() {
+        return {};
+    },
+});
+
+export default VsContainer;
+export type VsContainerInstance = InstanceType<typeof VsContainer>;
+</script>
+
+<style lang="scss" scoped src="./VsContainer.scss" />
