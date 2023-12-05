4cfb28190d8dd6aca77a56a160771fa50e0faf14
우수연 <sean123@pubg.com>
2023-12-05 18:41:45 +0900

    test: rebase and add new commit

--- packages/vlossom/vite.config.ts
+++ packages/vlossom/vite.config.ts
@@ -6,12 +6,9 @@ import path from 'path';
 export default defineConfig({
     plugins: [vue()],
     resolve: {
-        alias: [
-            {
-                find: '@',
-                replacement: path.resolve(__dirname, 'src'),
-            },
-        ],
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
     build: {
         lib: {
