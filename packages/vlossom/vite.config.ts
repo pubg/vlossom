16c4b4b02d351f6f62253cf3b84e6b3c39c35f13
Suyeon Woo <134579071+seaneez@users.noreply.github.com>
2023-12-07 17:23:30 +0900

    fix: fix test-config (#24)

--- packages/vlossom/vite.config.ts
+++ packages/vlossom/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +25,19 @@ export default defineConfig({
                     vue: 'Vue',
                 },
             },
+        },
+    },
+    // https://vitest.dev/config/
+    test: {
+        // 브라우저 환경에서 테스트하는 것을 명시
+        environment: 'jsdom',
+        root: fileURLToPath(new URL('./', import.meta.url)),
+        exclude: [...configDefaults.exclude, '**/stories/**', '**/*storybook/**'],
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            reportsDirectory: './coverage',
+            exclude: [...configDefaults.exclude, '**/stories/**', '**/*storybook/**'],
         },
     },
 });
