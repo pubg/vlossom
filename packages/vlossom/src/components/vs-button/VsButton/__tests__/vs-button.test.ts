f2ac4ca07067fc1044465dd4fb82a773b5a6db2f
gmldus <gmldus0407@naver.com>
2023-12-19 14:36:13 +0900

    feat(VsButton): fix test code

--- packages/vlossom/src/components/vs-button/VsButton/__tests__/vs-button.test.ts
+++ packages/vlossom/src/components/vs-button/VsButton/__tests__/vs-button.test.ts
@@ -3,34 +3,6 @@ import { mount } from '@vue/test-utils';
 import VsButton from '../VsButton.vue';
 
 describe('vs-button', () => {
-    describe('dense / large', () => {
-        it('dense 사이즈를 설정할 수 있다', () => {
-            // given
-            const wrapper = mount(VsButton, {
-                props: {
-                    dense: true,
-                },
-            });
-
-            // then
-            expect(wrapper.props('dense')).toBe(true);
-            expect(wrapper.classes('dense')).toBe(true);
-        });
-
-        it('large 사이즈를 설정할 수 있다', () => {
-            // given
-            const wrapper = mount(VsButton, {
-                props: {
-                    large: true,
-                },
-            });
-
-            // then
-            expect(wrapper.props('large')).toBe(true);
-            expect(wrapper.classes('large')).toBe(true);
-        });
-    });
-
     describe('disabled', () => {
         it('disabled를 설정할 수 있다.', () => {
             // given
@@ -47,7 +19,7 @@ describe('vs-button', () => {
     });
 
     describe('loading', () => {
-        it('loading을 설정할 수 있다.', () => {
+        it('loading인 경우에 rotate right 아이콘이 나타난다', () => {
             // given
             const wrapper = mount(VsButton, {
                 props: {
@@ -57,53 +29,7 @@ describe('vs-button', () => {
 
             // then
             expect(wrapper.props('loading')).toBe(true);
-            expect(wrapper.get('.loading'));
-            expect(wrapper.find('.content').exists()).toBe(false);
-        });
-    });
-
-    describe('outline', () => {
-        it('outline style을 설정할 수 있다.', () => {
-            // given
-            const wrapper = mount(VsButton, {
-                props: {
-                    outline: true,
-                },
-            });
-
-            // then
-            expect(wrapper.props('outline')).toBe(true);
-            expect(wrapper.classes('outline')).toBe(true);
-        });
-    });
-
-    describe('mobile-full', () => {
-        it('mobile-full prop을 설정할 수 있다', () => {
-            //given
-            const wrapper = mount(VsButton, {
-                props: {
-                    mobileFull: true,
-                },
-            });
-
-            //then
-            expect(wrapper.props('mobileFull')).toBe(true);
-            expect(wrapper.classes('mobile-full')).toBe(true);
-        });
-    });
-
-    describe('primary', () => {
-        it('primary style을 설정할 수 있다.', () => {
-            // given
-            const wrapper = mount(VsButton, {
-                props: {
-                    primary: true,
-                },
-            });
-
-            // then
-            expect(wrapper.props('primary')).toBe(true);
-            expect(wrapper.classes('primary')).toBe(true);
+            expect(wrapper.findComponent({ name: 'rotate-right' }).exists()).toBe(true);
         });
     });
 });
