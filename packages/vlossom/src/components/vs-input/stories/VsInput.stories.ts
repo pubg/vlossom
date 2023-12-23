fb2caba940a24aa8e9a9899e530beec8ded5f3c1
gmldus <gmldus0407@naver.com>
2023-12-24 01:10:29 +0900

    fix(VsInput): storybook test

--- packages/vlossom/src/components/vs-input/stories/VsInput.stories.ts
+++ packages/vlossom/src/components/vs-input/stories/VsInput.stories.ts
@@ -139,7 +139,7 @@ export const Prepend: Story = {
         template: `
             <vs-input v-model="value" v-bind="args">
                 <template #prepend-icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                    <svg aria-label="edit" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                         <path fill="currentColor" d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
                     </svg>
                 </template>
@@ -161,7 +161,7 @@ export const Append: Story = {
         template: `
             <vs-input v-model="value" v-bind="args">
                 <template #append-icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                    <svg aria-label="search" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                         <path fill="currentColor" d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
                     </svg>
                 </template>
