5ae2bad47570698a4f2d1bef1a72526f31ac15e0
gmldus <gmldus0407@naver.com>
2023-12-27 17:05:38 +0900

    feat(VsInput): fix story

--- packages/vlossom/src/components/vs-input/stories/VsInput.stories.ts
+++ packages/vlossom/src/components/vs-input/stories/VsInput.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
 import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
-import { ref } from 'vue';
 import { colorScheme, getMetaArguments } from '@/storybook/args';
 import VsInput, { InputType } from '../VsInput.vue';
 import { UIState } from '@/declaration/types';
@@ -12,10 +11,9 @@ const meta: Meta<typeof VsInput> = {
     render: (args: any) => ({
         components: { VsInput },
         setup() {
-            const value = ref('');
-            return { value, args };
+            return { args };
         },
-        template: '<vs-input v-model="value" v-bind="args" />',
+        template: '<vs-input v-bind="args" />',
     }),
     tags: ['autodocs'],
     args: {
@@ -40,19 +38,18 @@ export const ColorScheme: Story = {
     render: (args: any) => ({
         components: { VsInput },
         setup() {
-            const value = ref('');
-            return { value, args };
+            return { args };
         },
         template: `
             <div>
-                <vs-input v-model="value" v-bind="args" color-scheme="red" style="marginBottom: 10px"  />
-                <vs-input v-model="value" v-bind="args" color-scheme="amber" style="marginBottom: 10px" />
-                <vs-input v-model="value" v-bind="args" color-scheme="green" style="marginBottom: 10px" />
-                <vs-input v-model="value" v-bind="args" color-scheme="teal" style="marginBottom: 10px" />
-                <vs-input v-model="value" v-bind="args" color-scheme="blue" style="marginBottom: 10px" />
-                <vs-input v-model="value" v-bind="args" color-scheme="indigo" style="marginBottom: 10px" />
-                <vs-input v-model="value" v-bind="args" color-scheme="purple" style="marginBottom: 10px" />
-                <vs-input v-model="value" v-bind="args" color-scheme="pink" />
+                <vs-input v-bind="args" color-scheme="red" style="marginBottom: 10px"  />
+                <vs-input v-bind="args" color-scheme="amber" style="marginBottom: 10px" />
+                <vs-input v-bind="args" color-scheme="green" style="marginBottom: 10px" />
+                <vs-input v-bind="args" color-scheme="teal" style="marginBottom: 10px" />
+                <vs-input v-bind="args" color-scheme="blue" style="marginBottom: 10px" />
+                <vs-input v-bind="args" color-scheme="indigo" style="marginBottom: 10px" />
+                <vs-input v-bind="args" color-scheme="purple" style="marginBottom: 10px" />
+                <vs-input v-bind="args" color-scheme="pink" />
             </div>
         `,
     }),
@@ -116,11 +113,10 @@ export const Prepend: Story = {
     render: (args: any) => ({
         components: { VsInput },
         setup() {
-            const value = ref('');
-            return { value, args };
+            return { args };
         },
         template: `
-            <vs-input v-model="value" v-bind="args">
+            <vs-input v-bind="args">
                 <template #prepend-icon>
                     <svg aria-label="edit" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                         <path fill="currentColor" d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
@@ -138,11 +134,10 @@ export const Append: Story = {
     render: (args: any) => ({
         components: { VsInput },
         setup() {
-            const value = ref('');
-            return { value, args };
+            return { args };
         },
         template: `
-            <vs-input v-model="value" v-bind="args">
+            <vs-input v-bind="args">
                 <template #append-icon>
                     <svg aria-label="search" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                         <path fill="currentColor" d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
