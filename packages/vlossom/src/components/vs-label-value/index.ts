ee26cd6c65441abdca06e823dc59f2c2e85bd9a9
gmldus <gmldus0407@naver.com>
2024-01-03 14:14:33 +0900

    fix: build

--- packages/vlossom/src/components/vs-label-value/index.ts
+++ packages/vlossom/src/components/vs-label-value/index.ts
@@ -1,9 +1,10 @@
 import { VsComponent } from '@/declaration/types';
 import VsLabelValue from './VsLabelValue.vue';
+import type { VsLabelValueStyleSet } from './VsLabelValue.vue';
 
 type VsLabelValueInstance = InstanceType<typeof VsLabelValue>;
 
-export type { VsLabelValueInstance };
+export type { VsLabelValueInstance, VsLabelValueStyleSet };
 
 export default {
     name: VsComponent.VsLabelValue,
