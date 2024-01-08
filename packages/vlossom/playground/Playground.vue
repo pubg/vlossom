0cf3f173dc73f2bb7098dcdfb69322c5fc306ab1
Suyeon Woo <sean123@pubg.com>
2024-01-08 15:14:24 +0900

    WIP

--- packages/vlossom/playground/Playground.vue
+++ packages/vlossom/playground/Playground.vue
@@ -4,6 +4,11 @@
 
         Hello World
     </vs-page>
+    <vs-section style="border: solid 1px red">
+        <div class="box parent">
+            <div class="box child"></div>
+        </div>
+    </vs-section>
 </template>
 
 <script lang="ts">
@@ -14,3 +19,20 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.parent {
+    background-color: rgba(0, 0, 0, 1);
+    width: 100px;
+    height: 100px;
+}
+
+.child {
+    background-color: blue;
+    width: 50px;
+    height: 50px;
+    padding: 20px;
+
+    border: solid 5px rgba(255, 255, 0, 0.5);
+}
+</style>
