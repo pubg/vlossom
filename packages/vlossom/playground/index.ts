459cdbc69212a18737c7c03f42704c4b8e1f7072
smithoo <smithoo@pubg.com>
2024-01-11 16:48:58 +0900

    Add playground component typing

--- packages/vlossom/playground/index.ts
+++ packages/vlossom/playground/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createVlossom } from '../src';
+import '../src/components'; // for playground component typing
 import Playground from './Playground.vue';
 
 const app = createApp(Playground);
