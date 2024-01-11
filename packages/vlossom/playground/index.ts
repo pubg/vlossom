4918edea7bfbead18cc9e0d179a5e4a45a9fa222
smithoo <smithoo@pubg.com>
2024-01-11 18:15:59 +0900

    Remove unused import at playgound

--- packages/vlossom/playground/index.ts
+++ packages/vlossom/playground/index.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import { createVlossom } from '../src';
-import '../src/components'; // for playground component typing
 import Playground from './Playground.vue';
 
 const app = createApp(Playground);
