f1bf5e88216abf35dcbb11bdf0437be5c0787977
smithoo <smithoo@pubg.com>
2024-01-11 16:48:45 +0900

    Add vlossom global property type

--- packages/vlossom/src/index.ts
+++ packages/vlossom/src/index.ts
@@ -1,4 +1,11 @@
 import './styles/index.scss';
+import type { Vlossom } from './vlossom-framework';
+
+declare module 'vue' {
+    interface ComponentCustomProperties {
+        $vlossom: Vlossom;
+    }
+}
 
 export * from './vlossom-framework';
 export * from './components';
