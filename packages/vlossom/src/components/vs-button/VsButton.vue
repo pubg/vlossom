210ee8454875197aef8a42838ef523ebdd9d7b87
gmldus <gmldus0407@naver.com>
2024-01-12 17:03:27 +0900

    feat(chromatic): test

--- packages/vlossom/src/components/vs-button/VsButton.vue
+++ packages/vlossom/src/components/vs-button/VsButton.vue
@@ -37,7 +37,7 @@ export default defineComponent({
         primary: { type: Boolean, default: false },
     },
     setup(props) {
-        const { colorScheme, styleSet, dense, large, loading, mobileFull, outline, primary } = toRefs(props);
+        const { colorScheme, styleSet, dense, loading, mobileFull, outline, primary } = toRefs(props);
 
         const { computedColorScheme } = useColorScheme(name, colorScheme);
 
@@ -45,7 +45,7 @@ export default defineComponent({
 
         const classObj = computed(() => ({
             dense: dense.value,
-            large: large.value,
+            // large: large.value,
             loading: loading.value,
             'mobile-full': mobileFull.value,
             outline: outline.value,
