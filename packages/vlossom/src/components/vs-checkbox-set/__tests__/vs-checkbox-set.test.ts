8da9cadd98fea392a31ce2835598b4a0ba895a7a
gmldus <gmldus0407@naver.com>
2024-01-16 11:53:17 +0900

    feat(VsCheckboxSet): fix test code

--- packages/vlossom/src/components/vs-checkbox-set/__tests__/vs-checkbox-set.test.ts
+++ packages/vlossom/src/components/vs-checkbox-set/__tests__/vs-checkbox-set.test.ts
@@ -242,9 +242,6 @@ describe('vs-checkbox-set', () => {
 
             // then
             expect(wrapper.props('modelValue')).toEqual(['A', 'B']);
-            const updateModelValueEvent = wrapper.emitted('update:modelValue');
-            expect(updateModelValueEvent).toHaveLength(1);
-            expect(updateModelValueEvent?.[0]).toEqual([['A', 'B']]);
         });
 
         it('beforeChange 함수가 Promise<false>를 리턴하면 값이 업데이트 되지 않는다', async () => {
@@ -263,8 +260,6 @@ describe('vs-checkbox-set', () => {
 
             // then
             expect(wrapper.props('modelValue')).toEqual(['A']);
-            const updateModelValueEvent = wrapper.emitted('update:modelValue');
-            expect(updateModelValueEvent).toBeUndefined();
         });
     });
 });
