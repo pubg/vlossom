cfac0d3d951ccd2e5cfb8c49090920fb6bd9ab2f
박예린 <yerin.park@pubg.com>
2024-01-25 12:26:16 +0900

    fix: lint

--- packages/vlossom/src/composables/index.ts
+++ packages/vlossom/src/composables/index.ts
@@ -1,6 +1,6 @@
 export * from './color-scheme-composable';
 export * from './custom-style-composable';
-export * from './dom-attach-composables'
+export * from './dom-attach-composables';
 export * from './form-provide-composable';
 export * from './input-composable';
 export * from './input-form-composable';
