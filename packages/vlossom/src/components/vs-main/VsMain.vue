6cd306dc87d035d8bd6fd2da44e5b11a51a22ec8
gmldus <gmldus0407@naver.com>
2024-01-25 10:54:17 +0900

    feat(VsMain): add component (chromatic-build)

---
+++ packages/vlossom/src/components/vs-main/VsMain.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="vs-main">
+        <slot />
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { VsComponent } from '@/declaration';
+
+const name = VsComponent.VsMain;
+export default defineComponent({
+    name,
+    setup() {
+        return {};
+    },
+});
+</script>
+
+<style lang="scss" scoped src="./VsMain.scss" />
