f7bff1e23c4a05bd78fb8fa7b8e97d0907ad499f
smithoo <smithoo@pubg.com>
2024-01-30 16:27:59 +0900

    add input props test

--- packages/vlossom/src/composables/__tests__/input-composable.test.ts
+++ packages/vlossom/src/composables/__tests__/input-composable.test.ts
@@ -6,7 +6,27 @@ import { UIState } from '@/declaration';
 
 import type { StateMessage } from '@/declaration';
 
-describe('input composable', () => {
+describe('getInputProps', () => {
+    it('input component에 필요한 props들을 가져올 수 있다', () => {
+        // when
+        const props = getInputProps();
+
+        // then
+        expect(props).toMatchSnapshot();
+    });
+
+    it('input props 중 제외할 props를 정할 수 있다', () => {
+        // when
+        const props = getInputProps('label', 'messages');
+
+        // then
+        expect(props).toMatchSnapshot();
+        expect(props).not.toHaveProperty('label');
+        expect(props).not.toHaveProperty('messages');
+    });
+});
+
+describe('useInput composable', () => {
     const inputValue = ref('');
     let onMountedSpy = vi.fn();
     let onChangeSpy = vi.fn();
