7bef8eef4f38efdea803d687967ef1037df8ebf6
박예린 <yerin.park@pubg.com>
2024-02-01 18:52:05 +0900

    chore: change order of composables to alphabetical order

--- packages/vlossom/src/composables/index.ts
+++ packages/vlossom/src/composables/index.ts
@@ -1,6 +1,6 @@
+export * from './anchor-positioning-composable';
 export * from './color-scheme-composable';
 export * from './custom-style-composable';
-export * from './anchor-positioning-composable';
 export * from './form-provide-composable';
 export * from './input-composable';
 export * from './input-form-composable';
