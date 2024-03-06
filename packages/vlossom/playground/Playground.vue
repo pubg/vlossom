c07a6ab588f974488bb8c445d31ccc00d042d35a
Suyeon Woo <sean123@pubg.com>
2024-03-06 20:49:34 +0900

    feat(VsToast): revert playground

--- packages/vlossom/playground/Playground.vue
+++ packages/vlossom/playground/Playground.vue
@@ -1,58 +1,16 @@
 <template>
     <vs-page>
         <template #title>Vlossom Playground</template>
+
         Hello Vlossom!
-        <vs-button
-            @click="
-                $vs.toast.info('<h1>This is Toast This is Toast Info! 😀</h1>', {
-                    autoClose: false,
-                })
-            "
-            >TOP</vs-button
-        >
-        <vs-button
-            @click="
-                $vs.toast.error('This is Toast Error This iis Toast Error This is Toast Error!', {
-                    autoClose: false,
-                    placement: 'bottom',
-                    colorScheme: 'red',
-                })
-            "
-            >BOTTOM</vs-button
-        >
-        <vs-button
-            @click="
-                $vs.toast.success('This is Toast Successccess This is Toast Success This is Toast Success!', {
-                    autoClose: false,
-                    placement: 'top',
-                    align: 'start',
-                    colorScheme: 'green',
-                })
-            "
-            >LEFT</vs-button
-        >
-        <vs-button
-            @click="
-                $vs.toast.warn('This is Toast Warn! This iis Toast Warn This is Toast Warn', {
-                    autoClose: false,
-                    placement: 'top',
-                    align: 'end',
-                    colorScheme: 'orange',
-                })
-            "
-            >RIGHT</vs-button
-        >
     </vs-page>
 </template>
 
 <script lang="ts">
-// import { useVlossom } from '../src';
-
 export default {
     name: 'Playground',
     setup() {
-        // const { toast } = useVlossom();
-        // toast.info('Hello Vlossom!');
+        return {};
     },
 };
 </script>
