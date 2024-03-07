23f590c875a41ade8479cb7bb097a966370bdd0e
Suyeon Woo <sean123@pubg.com>
2024-03-07 16:28:11 +0900

    feat(VsToast): add optional chaining for closeButtonRef focus

--- packages/vlossom/src/components/vs-toast/VsToastView.vue
+++ packages/vlossom/src/components/vs-toast/VsToastView.vue
@@ -47,7 +47,7 @@ export default defineComponent({
 
             if (event.key === 'Tab' && event.shiftKey === false) {
                 event.preventDefault();
-                toastItemRefs.value[0].$refs.closeButtonRef.focus();
+                toastItemRefs.value[0].$refs.closeButtonRef?.focus();
             }
         }
 
