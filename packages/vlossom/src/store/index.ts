00a5f0e28ef3cc919447d983a58ef25b2b422f06
Suyeon Woo <sean123@pubg.com>
2024-03-07 18:23:54 +0900

    feat(VsToast): modify store

--- packages/vlossom/src/store/index.ts
+++ packages/vlossom/src/store/index.ts
@@ -3,9 +3,16 @@ import { OptionStore } from './option-store';
 import { ToastStore } from './toast-store';
 
 export class VsStore {
-    public dialog = new DialogStore();
     public option = new OptionStore();
-    public _toast: ToastStore | null = null;
+    private _dialog: DialogStore | null = null;
+    private _toast: ToastStore | null = null;
+
+    public get dialog() {
+        if (!this._dialog) {
+            this._dialog = new DialogStore();
+        }
+        return this._dialog;
+    }
 
     public get toast() {
         if (!this._toast) {
