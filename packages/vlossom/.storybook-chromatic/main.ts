e8f605c80191ebb46ece48fc4e7a9e183b7d5c6b
smithoo <smithoo@pubg.com>
2024-04-13 00:05:36 +0900

    seperate storybook-chromatic build

---
+++ packages/vlossom/.storybook-chromatic/main.ts
@@ -0,0 +1,21 @@
+import { dirname, join } from 'path';
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+    stories: ['../src/**/*.chromatic.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        getAbsolutePath('@storybook/addon-links'),
+        getAbsolutePath('@storybook/addon-essentials'),
+        getAbsolutePath('@storybook/addon-interactions'),
+        getAbsolutePath('@chromatic-com/storybook'),
+    ],
+    framework: {
+        name: getAbsolutePath('@storybook/vue3-vite'),
+        options: {},
+    },
+};
+export default config;
