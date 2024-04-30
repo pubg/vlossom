8cf2f747c0dd6c8b283aa9bc52c3927876fbd8f4
Suyeon Woo <sean123@pubg.com>
2024-04-30 14:27:04 +0900

    remove currentColor option

--- packages/vlossom/src/icons/icons.ts
+++ packages/vlossom/src/icons/icons.ts
@@ -8,8 +8,7 @@ export const iconSvgs = {
         </svg>`,
     check: `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-            <path fill="currentColor"
-            d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
+            <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
         </svg>
 	`,
     // check_box (fill)
