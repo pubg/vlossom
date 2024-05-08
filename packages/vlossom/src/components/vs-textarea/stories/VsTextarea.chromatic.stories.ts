25d1e7d34a8d860017921f1285f42cf21473ae3a
Suyeon Woo <sean123@pubg.com>
2024-05-08 13:54:18 +0900

    feat(VsTextarea): add chromatic stories

---
+++ packages/vlossom/src/components/vs-textarea/stories/VsTextarea.chromatic.stories.ts
@@ -0,0 +1,46 @@
+import { colorScheme, getMetaArguments, state } from '@/storybook';
+import { UIState } from '@/declaration';
+import VsTextarea from './../VsTextarea.vue';
+
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta: Meta<typeof VsTextarea> = {
+    title: 'Chromatic/Input Components/VsTextarea',
+    component: VsTextarea,
+    render: (args: any) => ({
+        components: { VsTextarea },
+        setup() {
+            const messages = [{ state: UIState.Success, text: 'This is success message' }];
+            return { args, messages };
+        },
+        template: `
+            <div>
+                <vs-textarea v-bind="args" label="Textarea" :style="{ marginBottom: '12px' }"/>
+
+                <vs-textarea  v-bind="args" label="Required Textarea" required :style="{ marginBottom: '12px' }"/>
+
+                <vs-textarea v-bind="args" label="Readonly Textarea" readonly :style="{ marginBottom: '12px' }"/>
+
+                <vs-textarea v-bind="args" label="Disabled Textarea" disabled :style="{ marginBottom: '12px' }"/>
+
+                <vs-textarea v-bind="args" label="Textarea with Messages"  :messages="messages" :style="{ marginBottom: '12px' }"/>
+                
+                <vs-textarea v-bind="args" label="Dense Textarea" dense/>
+            </div>
+		`,
+    }),
+    tags: ['autodocs'],
+    args: {
+        placeholder: 'enter text',
+    },
+    argTypes: {
+        colorScheme,
+        state,
+    },
+};
+
+meta.args = getMetaArguments(VsTextarea.props, meta.args);
+export default meta;
+type Story = StoryObj<typeof VsTextarea>;
+
+export const Default: Story = {};
