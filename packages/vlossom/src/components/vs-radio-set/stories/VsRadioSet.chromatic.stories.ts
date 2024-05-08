8d8f8ef0c4191bbb2b486e72b26f32d39c1e09fc
Suyeon Woo <sean123@pubg.com>
2024-05-08 13:33:14 +0900

    feat(VsRadioSet): modify style, add chromatic stories

---
+++ packages/vlossom/src/components/vs-radio-set/stories/VsRadioSet.chromatic.stories.ts
@@ -0,0 +1,47 @@
+import { colorScheme, getMetaArguments, state } from '@/storybook';
+import { UIState } from '@/declaration';
+import VsRadioSet from '../VsRadioSet.vue';
+
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta: Meta<typeof VsRadioSet> = {
+    title: 'Chromatic/Input Components/VsRadioSet',
+    component: VsRadioSet,
+    render: (args: any) => ({
+        components: { VsRadioSet },
+        setup() {
+            const messages = [{ state: UIState.Success, text: 'This is success message' }];
+            return { args, messages };
+        },
+        template: `
+            <div>
+                <vs-radio-set v-bind="args" label="Radio Set" :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Required Radio Set" required :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Readonly Radio Set" readonly :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Disabled Radio Set" disabled :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Radio Set with Messages" :messages="messages" :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Vertical Radio Set" vertical/>
+            </div>
+		`,
+    }),
+    tags: ['autodocs'],
+    argTypes: {
+        colorScheme,
+        state,
+    },
+    args: {
+        name: 'fruit',
+        options: ['Apple', 'Banana', 'Carrot'],
+    },
+};
+
+meta.args = getMetaArguments(VsRadioSet.props, meta.args);
+export default meta;
+type Story = StoryObj<typeof VsRadioSet>;
+
+export const Default: Story = {};
