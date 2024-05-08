91b3e864b94468519144db0b7551c62cb0b6b3b5
Suyeon Woo <sean123@pubg.com>
2024-05-08 16:32:18 +0900

    feat(VsButton): add chromatic stories

---
+++ packages/vlossom/src/components/vs-button/stories/VsButton.chromatic.stories.ts
@@ -0,0 +1,40 @@
+import { colorScheme } from '@/storybook';
+import VsButton from './../VsButton.vue';
+
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta: Meta<typeof VsButton> = {
+    title: 'Chromatic/Base Components/VsButton',
+    component: VsButton,
+    render: (args: any) => ({
+        components: { VsButton },
+        setup() {
+            return { args };
+        },
+        template: `
+            <div style="display:flex; align-items:center;">
+                <vs-button v-bind="args">Button</vs-button>
+
+                <vs-button v-bind="args" outline>Button</vs-button>
+				
+                <vs-button v-bind="args" primary>Button</vs-button>
+
+                <vs-button v-bind="args" disabled>Button</vs-button>
+				
+                <vs-button v-bind="args" loading>Button</vs-button>
+
+                <vs-button v-bind="args" dense>Button</vs-button>
+
+                <vs-button v-bind="args" large>Button</vs-button>
+            </div>`,
+    }),
+    tags: ['autodocs'],
+    argTypes: {
+        colorScheme,
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof VsButton>;
+
+export const Default: Story = {};
