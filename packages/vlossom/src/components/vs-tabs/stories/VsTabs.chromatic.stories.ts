c37f271fa6b7d235a7dd394cd6ea9fce4b3d3dcd
Suyeon Woo <sean123@pubg.com>
2024-05-09 17:48:42 +0900

    feat(VsTabs): add chromatic stories

---
+++ packages/vlossom/src/components/vs-tabs/stories/VsTabs.chromatic.stories.ts
@@ -0,0 +1,45 @@
+import { colorScheme } from '@/storybook';
+import VsTabs from '../VsTabs.vue';
+import { SCROLL_BUTTONS } from '../types';
+import { tabLength, disabledArgTypes, tabs } from './constants';
+
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta: Meta<typeof VsTabs> = {
+    title: 'Chromatic/Layout Components/VsTabs',
+    component: VsTabs,
+    render: (args: any) => ({
+        components: { VsTabs },
+        setup() {
+            const disabledArrs = [...Array(tabLength).keys()];
+            return { args, tabs, disabledArrs };
+        },
+        template: `
+            <div>
+                <vs-tabs v-bind="args" :style="{ marginBottom: '20px' }"/>
+                				
+                <vs-tabs v-bind="args" :disabled="disabledArrs" :style="{ marginBottom: '20px' }"/>
+                
+                <vs-tabs v-bind="args" scrollButtons="show" :style="{ marginBottom: '20px' }"/>
+                
+                <vs-tabs v-bind="args" dense/>
+            </div>`,
+    }),
+    tags: ['autodocs'],
+    args: {
+        tabs,
+    },
+    argTypes: {
+        colorScheme,
+        disabled: disabledArgTypes,
+        scrollButtons: {
+            control: 'select',
+            options: SCROLL_BUTTONS,
+        },
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof VsTabs>;
+
+export const Default: Story = {};
