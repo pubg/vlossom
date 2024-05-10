d4c2168e76b975fb4c39d0d439bc8c35fa75f265
Suyeon Woo <sean123@pubg.com>
2024-05-10 15:20:39 +0900

    feat(VsDrawer): add chromatic stories

---
+++ packages/vlossom/src/components/vs-drawer/stories/DrawerCloseBotton.ts
@@ -0,0 +1,8 @@
+import { VsIcon } from '@/icons';
+
+export const DrawerCloseButton = {
+    components: { VsIcon },
+    template: `
+        <vs-icon icon="close" style="cursor: pointer" />
+    `,
+};
---
+++ packages/vlossom/src/components/vs-drawer/stories/constants.ts
@@ -0,0 +1,19 @@
+const contentStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '1rem',
+};
+
+const containerStyle = {
+    backgroundColor: 'rgba(0, 0, 0, 0.02)',
+    border: '1px solid rgb(240, 240, 240)',
+    borderRadius: '4px',
+    color: 'var(--vs-comp-font)',
+    height: '800px',
+    overflow: 'hidden',
+    position: 'relative',
+    padding: '2rem',
+    width: '600px',
+};
+
+export { contentStyle, containerStyle };
