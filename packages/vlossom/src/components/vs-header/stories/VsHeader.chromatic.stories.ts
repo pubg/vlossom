4cb6bf281a21aed876e7841b099047dd690bcfe7
Suyeon Woo <sean123@pubg.com>
2024-05-10 15:03:13 +0900

    feat(VsHeader): add chromatic stories

---
+++ packages/vlossom/src/components/vs-header/stories/VsHeader.chromatic.stories.ts
@@ -0,0 +1,50 @@
+import { colorScheme, align, cssPosition, LOREM_IPSUM } from '@/storybook';
+import VsHeader from './../VsHeader.vue';
+
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta: Meta<typeof VsHeader> = {
+    title: 'Chromatic/Layout Components/VsHeader',
+    component: VsHeader,
+    render: (args: any) => ({
+        components: { VsHeader },
+        setup() {
+            return { args };
+        },
+        template: `
+            <div>
+                <div style="background-color:#fff; position: relative; width: 100%; margin-bottom: 12px;">
+                    <vs-header height="80px" v-bind="args" verticalAlign="start"> This is Header Content </vs-header>
+                    <div style="padding-top:80px;">${LOREM_IPSUM}</div>
+                </div>
+
+                <div style="background-color:#fff; position: relative; width: 100%; margin-bottom: 12px;">
+                    <vs-header height="80px" v-bind="args"> This is Header Content </vs-header>
+                    <div style="padding-top:80px;">${LOREM_IPSUM}</div>
+                </div>
+
+                <div style="background-color:#fff; position: relative; width: 100%; margin-bottom: 12px;">
+                    <vs-header height="80px" v-bind="args" verticalAlign="end"> This is Header Content </vs-header>
+                    <div style="padding-top:80px;">${LOREM_IPSUM}</div>
+                </div>
+
+                <div style="background-color:#fff; position: relative; width: 100%; margin-bottom: 12px;">
+                    <vs-header primary height="80px" v-bind="args"> This is Header Content </vs-header>
+                    <div style="padding-top:80px;">${LOREM_IPSUM}</div>
+                </div>
+            </div>`,
+    }),
+    tags: ['autodocs'],
+    args: {
+        position: 'absolute',
+    },
+    argTypes: {
+        colorScheme,
+        verticalAlign: align,
+        position: cssPosition,
+    },
+};
+export default meta;
+type Story = StoryObj<typeof VsHeader>;
+
+export const Default: Story = {};
