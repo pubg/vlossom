a3107ac209b2a406f962f64ef747d9e03d163728
Suyeon Woo <134579071+seaneez@users.noreply.github.com>
2024-05-14 11:06:07 +0900

    feat(input components): modify style, add chromatic stories (#176)

---
+++ packages/vlossom/src/components/vs-radio-set/stories/VsRadioSet.chromatic.stories.ts
@@ -0,0 +1,50 @@
+import { colorScheme, getMetaArguments, state } from '@/storybook';
+import { UIState } from '@/declaration';
+import VsRadioSet from '../VsRadioSet.vue';
+
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { ref } from 'vue';
+
+const meta: Meta<typeof VsRadioSet> = {
+    title: 'Chromatic/Input Components/VsRadioSet',
+    component: VsRadioSet,
+    render: (args: any) => ({
+        components: { VsRadioSet },
+        setup() {
+            const messages = [{ state: UIState.Success, text: 'This is success message' }];
+            const modelValue = ref('Apple');
+            return { args, messages, modelValue };
+        },
+        template: `
+            <div>
+                <vs-radio-set v-bind="args" label="Radio Set" :style="{ marginBottom: '12px' }"/>
+                
+                <vs-radio-set v-bind="args" v-model="modelValue" label="Radio Set" :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Required Radio Set" required :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Readonly Radio Set" readonly :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Disabled Radio Set" disabled :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Radio Set with Messages" :messages="messages" :style="{ marginBottom: '12px' }"/>
+
+                <vs-radio-set v-bind="args" label="Vertical Radio Set" vertical/>
+            </div>
+		`,
+    }),
+    argTypes: {
+        colorScheme,
+        state,
+    },
+    args: {
+        name: 'fruit',
+        options: ['Apple', 'Banana', 'Carrot'],
+    },
+};
+
+meta.args = getMetaArguments(VsRadioSet.props, meta.args);
+export default meta;
+type Story = StoryObj<typeof VsRadioSet>;
+
+export const Default: Story = {};
--- packages/vlossom/src/storybook/template.ts
+++ packages/vlossom/src/storybook/template.ts
@@ -1,13 +1,13 @@
 import { COLORS, UIState } from '@/declaration';
 
 export const LOREM_IPSUM = `
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-    Aenean commodo ligula eget dolor. Aenean massa.
-    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-    Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.`;
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+Aenean commodo ligula eget dolor. Aenean massa.
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.`;
 
 export function getStateTemplate(templateStr: string) {
     const states = [UIState.Info, UIState.Success, UIState.Warning, UIState.Error];
