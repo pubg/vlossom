4a764009fbe5360940677a580942ee1ce8c943f8
smithoo <smithoo@pubg.com>
2024-05-21 17:54:34 +0900

    fix textarea convertValue (chromatic-build)

--- packages/vlossom/src/components/vs-textarea/VsTextarea.vue
+++ packages/vlossom/src/components/vs-textarea/VsTextarea.vue
@@ -119,7 +119,7 @@ export default defineComponent({
                 return '';
             }
 
-            return modifyStringValue(v);
+            return modifyStringValue(v.toString());
         }
 
         function onClear() {
