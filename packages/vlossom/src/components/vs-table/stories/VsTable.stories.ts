897f7b935634493f7a9e1dcb9deedeccce853491
박예린 <yerin.park@pubg.com>
2024-05-29 10:55:34 +0900

    fix(VsTable): fix story args binding

--- packages/vlossom/src/components/vs-table/stories/VsTable.stories.ts
+++ packages/vlossom/src/components/vs-table/stories/VsTable.stories.ts
@@ -117,9 +117,9 @@ export const Search: Story = {
         },
         template: `
             <div>
-                <vs-input v-model="search" placeholder="Search" style="padding: 0.5rem">
+                <vs-input v-bind="args" v-model="search" placeholder="Search">
                     <template #prepend>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" style="margin-left: 0.5rem">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#FFFFFF">
                             <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
                         </svg>
                     </template>
@@ -165,10 +165,10 @@ export const Filter: Story = {
         },
         template: `
             <div>
-                <vs-block style="margin-bottom: 0.8rem">
+                <vs-block v-bind="args" style="margin-bottom: 0.8rem">
                     <h3 style="margin-bottom: 0.5rem">Filter</h3>
-                    <vs-select v-model="id" :options="options" label="id" no-clear style="margin-bottom: 0.5rem"/>
-                    <vs-switch v-model="checked" label="checked" true-label="true" false-label="false" no-label no-message/>
+                    <vs-select v-bind="args"  v-model="id" :options="options" label="id" no-clear style="margin-bottom: 0.5rem"/>
+                    <vs-switch v-model="checked" label="checked" true-label="true" false-label="false" no-message/>
                 </vs-block>
                 <vs-table v-bind="args" :filter="filter"/>
             </div>
