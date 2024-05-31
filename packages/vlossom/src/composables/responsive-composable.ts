90cd6939eaa0d9631d22cf806e5f7931d8be6f9d
Suyeon Woo <134579071+seaneez@users.noreply.github.com>
2024-05-31 16:39:14 +0900

    feat(responsive-composable): modify width, grid types (#192)

--- packages/vlossom/src/composables/responsive-composable.ts
+++ packages/vlossom/src/composables/responsive-composable.ts
@@ -3,66 +3,94 @@ import { utils } from '@/utils';
 
 import type { Breakpoints } from '@/declaration';
 
+function hasValue(value: any): value is string | number | Breakpoints {
+    return value !== undefined && value !== null && value !== '';
+}
+
+function convertWidth(width: string | number) {
+    if (typeof width === 'string' && isNaN(Number(width))) {
+        return width;
+    } else {
+        return `${width}px`;
+    }
+}
+
 export function getResponsiveProps() {
     return {
-        width: { type: [String, Object] as PropType<string | Breakpoints>, default: null },
-        grid: { type: Object as PropType<Breakpoints>, default: () => ({}) },
+        width: { type: [String, Number, Object] as PropType<string | number | Breakpoints>, default: null },
+        grid: { type: [String, Number, Object] as PropType<string | number | Breakpoints>, default: null },
     };
 }
 
-export function useResponsive(width: Ref<string | Breakpoints | null>, grid: Ref<Breakpoints>) {
+export function useResponsive(
+    width: Ref<string | number | Breakpoints | null>,
+    grid: Ref<string | number | Breakpoints | null>,
+) {
     const responsiveClasses: ComputedRef<string[]> = computed(() => {
         const classes: string[] = ['vs-responsive'];
 
-        if (width.value && utils.object.isPlainObject(width.value)) {
+        if (hasValue(width.value) && utils.object.isPlainObject(width.value)) {
             const { sm, md, lg, xl } = width.value;
             const widthClasses = [
-                ...(sm ? ['vs-width-sm'] : []),
-                ...(md ? ['vs-width-md'] : []),
-                ...(lg ? ['vs-width-lg'] : []),
-                ...(xl ? ['vs-width-xl'] : []),
+                ...(hasValue(sm) ? ['vs-width-sm'] : []),
+                ...(hasValue(md) ? ['vs-width-md'] : []),
+                ...(hasValue(lg) ? ['vs-width-lg'] : []),
+                ...(hasValue(xl) ? ['vs-width-xl'] : []),
             ];
             classes.push(...widthClasses);
         }
 
-        const { sm, md, lg, xl } = grid.value;
-        const gridClasses = [
-            ...(sm ? ['vs-grid-sm'] : []),
-            ...(md ? ['vs-grid-md'] : []),
-            ...(lg ? ['vs-grid-lg'] : []),
-            ...(xl ? ['vs-grid-xl'] : []),
-        ];
-        classes.push(...gridClasses);
+        if (hasValue(grid.value) && utils.object.isPlainObject(grid.value)) {
+            const { sm, md, lg, xl } = grid.value;
+            const gridClasses = [
+                ...(hasValue(sm) ? ['vs-grid-sm'] : []),
+                ...(hasValue(md) ? ['vs-grid-md'] : []),
+                ...(hasValue(lg) ? ['vs-grid-lg'] : []),
+                ...(hasValue(xl) ? ['vs-grid-xl'] : []),
+            ];
+            classes.push(...gridClasses);
+        }
 
         return classes;
     });
 
     const responsiveStyles: ComputedRef<Record<string, string>> = computed(() => {
         const styles: Record<string, string> = {};
+        if (hasValue(width.value)) {
+            if (utils.object.isPlainObject(width.value)) {
+                const { base, sm, md, lg, xl } = width.value;
+                const widthStyles = {
+                    ...(hasValue(base) && { ['--vs-width-base']: convertWidth(base) }),
+                    ...(hasValue(sm) && { ['--vs-width-sm']: convertWidth(sm) }),
+                    ...(hasValue(md) && { ['--vs-width-md']: convertWidth(md) }),
+                    ...(hasValue(lg) && { ['--vs-width-lg']: convertWidth(lg) }),
+                    ...(hasValue(xl) && { ['--vs-width-xl']: convertWidth(xl) }),
+                };
 
-        if (width.value && utils.object.isPlainObject(width.value)) {
-            const { base, sm, md, lg, xl } = width.value;
-            const widthStyles = {
-                ...(base && { ['--vs-width-base']: base?.toString() }),
-                ...(sm && { ['--vs-width-sm']: sm?.toString() }),
-                ...(md && { ['--vs-width-md']: md?.toString() }),
-                ...(lg && { ['--vs-width-lg']: lg?.toString() }),
-                ...(xl && { ['--vs-width-xl']: xl?.toString() }),
-            };
-            Object.assign(styles, widthStyles);
-        } else if (typeof width.value === 'string') {
-            styles['width'] = width.value;
+                Object.assign(styles, widthStyles);
+            } else {
+                styles['width'] = convertWidth(width.value);
+            }
         }
 
-        const { base, sm, md, lg, xl } = grid.value;
-        const gridStyles = {
-            ...(base && { ['--vs-grid-base']: base?.toString() }),
-            ...(sm && { ['--vs-grid-sm']: sm?.toString() }),
-            ...(md && { ['--vs-grid-md']: md?.toString() }),
-            ...(lg && { ['--vs-grid-lg']: lg?.toString() }),
-            ...(xl && { ['--vs-grid-xl']: xl?.toString() }),
-        };
-        Object.assign(styles, gridStyles);
+        if (hasValue(grid.value)) {
+            if (utils.object.isPlainObject(grid.value)) {
+                const { base, sm, md, lg, xl } = grid.value;
+                const gridStyles = {
+                    ...(hasValue(base) && { ['--vs-grid-base']: base?.toString() }),
+                    ...(hasValue(sm) && { ['--vs-grid-sm']: sm?.toString() }),
+                    ...(hasValue(md) && { ['--vs-grid-md']: md?.toString() }),
+                    ...(hasValue(lg) && { ['--vs-grid-lg']: lg?.toString() }),
+                    ...(hasValue(xl) && { ['--vs-grid-xl']: xl?.toString() }),
+                };
+                Object.assign(styles, gridStyles);
+            } else {
+                const gridStyles = {
+                    '--vs-grid-base': grid.value?.toString(),
+                };
+                Object.assign(styles, gridStyles);
+            }
+        }
 
         return styles;
     });
