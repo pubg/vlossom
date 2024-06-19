626d71c4bc1b66e28fa88f9d509f90c7a8182d04
박예린 <yerin.park@pubg.com>
2024-06-19 17:15:49 +0900

    feat: remove unnecessary condition check (chromatic-build)

--- packages/vlossom/src/components/vs-checkbox-set/vs-checkbox-set-rules.ts
+++ packages/vlossom/src/components/vs-checkbox-set/vs-checkbox-set-rules.ts
@@ -7,21 +7,11 @@ export function useVsCheckboxSetRules(required: Ref<boolean>, max: Ref<number | 
 
     function maxCheck(v: any[]) {
         const limit = Number(max.value);
-
-        if (isNaN(limit) || limit > Number.MAX_SAFE_INTEGER) {
-            return '';
-        }
-
         return v && v.length > limit ? 'max number of items: ' + max.value : '';
     }
 
     function minCheck(v: any[]) {
         const limit = Number(min.value);
-
-        if (isNaN(limit) || limit < Number.MIN_SAFE_INTEGER) {
-            return '';
-        }
-
         return v && v.length < limit ? 'min number of items: ' + min.value : '';
     }
 
