eaaef9050ce550978c8d4d5d294cf912d5a9645f
smithoo <smithoo@pubg.com>
2024-06-28 18:54:58 +0900

    update vs-avartar style

--- packages/vlossom/src/components/vs-avatar/types.ts
+++ packages/vlossom/src/components/vs-avatar/types.ts
@@ -1,9 +1,11 @@
 export interface VsAvatarStyleSet {
     backgroundColor?: string;
+    border?: string;
     borderRadius?: string;
     fontColor?: string;
     fontSize?: string;
     fontWeight?: string | number;
     height?: string;
+    objectFit?: 'cover' | 'fill' | 'contain' | 'none' | 'scale-down';
     width?: string;
 }
