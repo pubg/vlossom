dfaf53b56ed2245661e6ff0dd73e62d3501318aa
smithoo <smithoo@pubg.com>
2024-07-01 17:53:40 +0900

    update vs-button style

--- packages/vlossom/src/components/vs-button/VsButton.vue
+++ packages/vlossom/src/components/vs-button/VsButton.vue
@@ -35,13 +35,13 @@ export default defineComponent({
         disabled: { type: Boolean, default: false },
         large: { type: Boolean, default: false },
         loading: { type: Boolean, default: false },
-        mobileFull: { type: Boolean, default: false },
         outline: { type: Boolean, default: false },
         primary: { type: Boolean, default: false },
+        responsive: { type: Boolean, default: false },
         state: { type: String as PropType<UIState>, default: UIState.Idle },
     },
     setup(props) {
-        const { colorScheme, styleSet, dense, large, loading, mobileFull, outline, primary, state } = toRefs(props);
+        const { colorScheme, styleSet, dense, large, loading, responsive, outline, primary, state } = toRefs(props);
 
         const { computedColorScheme } = useColorScheme(name, colorScheme);
 
@@ -53,9 +53,9 @@ export default defineComponent({
             dense: dense.value,
             large: large.value,
             loading: loading.value,
-            'mobile-full': mobileFull.value,
             outline: outline.value,
             primary: primary.value,
+            responsive: responsive.value,
         }));
 
         return {
