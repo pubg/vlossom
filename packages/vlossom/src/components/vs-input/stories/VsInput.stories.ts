90f9a93c3fdcff71f8a761abf79fe25c27327ad0
박예린 <yerin.park@pubg.com>
2024-08-06 19:11:49 +0900

    fix lint

--- packages/vlossom/src/components/vs-input/stories/VsInput.stories.ts
+++ packages/vlossom/src/components/vs-input/stories/VsInput.stories.ts
@@ -253,7 +253,7 @@ export const StyleSet: Story = {
             backgroundColor: '#eef1ff',
             fontColor: '#4851aa',
             border: '1px solid #4851aa',
-            padding: '2rem'
+            padding: '2rem',
         },
     },
 };
