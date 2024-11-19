d137472e529de284c1451a291b6d8d96c9f1380a
smithoo <smithoo@pubg.com>
2024-11-19 17:32:02 +0900

    remove useless icon

--- packages/vlossom/src/icons/icons.ts
+++ packages/vlossom/src/icons/icons.ts
@@ -11,21 +11,6 @@ export const iconSvgs = {
             <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
         </svg>
 	`,
-    // check_box (fill)
-    checkboxChecked: `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-            <path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Z"/>
-        </svg>`,
-    // indeterminate_check_box (fill)
-    checkboxIndeterminate: `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-            <path d="M280-440h400v-80H280v80Zm-80 320q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Z"/>
-        </svg>`,
-    // check_box_outline_blank
-    checkboxUnchecked: `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Z"/>
-        </svg>`,
     close: `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
             <path
