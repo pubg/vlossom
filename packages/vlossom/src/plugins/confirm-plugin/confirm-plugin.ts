a2f1adc4bee3cccb18b3f0b90949a4bdf68f972f
smithoo <smithoo@pubg.com>
2024-11-25 17:44:33 +0900

    remove confirm component (will be replaced with render function)

--- packages/vlossom/src/plugins/confirm-plugin/confirm-plugin.ts
+++ packages/vlossom/src/plugins/confirm-plugin/confirm-plugin.ts
@@ -1,25 +1,25 @@
 import { render, h } from 'vue';
 import { store } from '@/stores';
 import { utils } from '@/utils';
-import VsConfirm from '@/components/vs-confirm/VsConfirm.vue';
+// import VsConfirm from '@/nodes/vs-confirm/VsConfirm.vue';
 
 import type { ConfirmOptions, ConfirmPlugin } from './types';
 
-function renderConfirm(text: string, confirmOptions: ConfirmOptions) {
-    const body = document?.body;
-    if (!body) {
-        utils.log.error('vs-confirm', 'body not found');
-        return;
-    }
+// function renderConfirm(text: string, confirmOptions: ConfirmOptions) {
+//     const body = document?.body;
+//     if (!body) {
+//         utils.log.error('vs-confirm', 'body not found');
+//         return;
+//     }
 
-    const confirmView = h(VsConfirm, { text, ...confirmOptions });
-    const confirmViewRoot = document.createElement('div');
-    render(confirmView, confirmViewRoot);
-}
+//     const confirmView = h(VsConfirm, { text, ...confirmOptions });
+//     const confirmViewRoot = document.createElement('div');
+//     render(confirmView, confirmViewRoot);
+// }
 
 export const confirmPlugin: ConfirmPlugin = {
     open: (text: string, confirmOptions: ConfirmOptions = {}): Promise<boolean> => {
-        renderConfirm(text, confirmOptions);
+        // renderConfirm(text, confirmOptions);
 
         return new Promise((resolve) => {
             store.confirm.setResolve(resolve);
