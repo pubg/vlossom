b7879e7fa4fe9abf7aa8c31c67d9fa164e4ab633
박예린 <yerin.park@pubg.com>
2025-01-02 14:21:10 +0900

    fix vs-overlay query select

--- packages/vlossom/src/composables/overlay-dom-composable.ts
+++ packages/vlossom/src/composables/overlay-dom-composable.ts
@@ -1,13 +1,13 @@
 import { VS_OVERLAY } from '@/declaration';
 
 export function useOverlayDom() {
-    function appendOverlayDom(target: string = VS_OVERLAY, zIndex: number = 10000) {
-        if (document.querySelector(target)) {
+    function appendOverlayDom(targetId: string = VS_OVERLAY, zIndex: number = 10000) {
+        if (document.getElementById(targetId)) {
             return;
         }
 
         const overlay = document.createElement('div');
-        overlay.setAttribute('id', target);
+        overlay.setAttribute('id', targetId);
         overlay.style.position = 'absolute';
         overlay.style.top = '0';
         overlay.style.left = '0';
