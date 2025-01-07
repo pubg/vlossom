5cb595827d2dc055e61f3017a0deb6af2e2e2500
smithoo <smithoo@pubg.com>
2025-01-07 14:04:42 +0900

    fix(VsModal): use modal-plugin at vs-modal component (#310)

--- packages/vlossom/src/components/vs-modal/VsModalView.vue
+++ packages/vlossom/src/components/vs-modal/VsModalView.vue
@@ -1,28 +1,30 @@
 <template>
-    <vs-modal-node
-        v-for="modal in modals"
-        :key="modal.id"
-        :color-scheme="modal.colorScheme"
-        :style-set="modal.styleSet"
-        :container="container"
-        :dim-close="modal.dimClose"
-        :dimmed="modal.dimmed"
-        :esc-close="modal.escClose"
-        :focus-lock="modal.focusLock"
-        :hide-scroll="modal.hideScroll"
-        :id="modal.id"
-        :initial-focus-ref="modal.initialFocusRef"
-        :size="modal.size"
-        :callbacks="modal.callbacks"
-    >
-        <template #header v-if="modal.header">
-            <vs-content-renderer :content="modal.header" />
-        </template>
-        <vs-content-renderer v-if="modal.component" :content="modal.component" />
-        <template #footer v-if="modal.footer">
-            <vs-content-renderer :content="modal.footer" />
-        </template>
-    </vs-modal-node>
+    <div class="vs-modal-view" :class="{ 'vs-modal-fixed': isFixed }" :id="wrapperId">
+        <vs-modal-node
+            v-for="modal in modals"
+            :key="modal.id"
+            :color-scheme="modal.colorScheme"
+            :style-set="modal.styleSet"
+            :container="container"
+            :dim-close="modal.dimClose"
+            :dimmed="modal.dimmed"
+            :esc-close="modal.escClose"
+            :focus-lock="modal.focusLock"
+            :hide-scroll="modal.hideScroll"
+            :id="modal.id"
+            :initial-focus-ref="modal.initialFocusRef"
+            :size="modal.size"
+            :callbacks="modal.callbacks"
+        >
+            <template #header v-if="modal.header">
+                <vs-content-renderer :content="modal.header" />
+            </template>
+            <vs-content-renderer v-if="modal.component" :content="modal.component" />
+            <template #footer v-if="modal.footer">
+                <vs-content-renderer :content="modal.footer" />
+            </template>
+        </vs-modal-node>
+    </div>
 </template>
 
 <script lang="ts">
@@ -30,7 +32,7 @@ import { computed, defineComponent, toRefs } from 'vue';
 import { store } from '@/stores';
 import { useContentRenderer } from '@/composables';
 import VsModalNode from '@/components/vs-modal/VsModalNode.vue';
-import VsContentRenderer from './../vs-content-renderer/VsContentRenderer.vue';
+import VsContentRenderer from '@/components/vs-content-renderer/VsContentRenderer.vue';
 
 export default defineComponent({
     props: {
@@ -45,7 +47,13 @@ export default defineComponent({
 
         const { getRenderedContent } = useContentRenderer();
 
-        return { modals, getRenderedContent };
+        const wrapperId = computed(() => `vs-modal-${container.value.replace('#', '').replace('.', '')}`);
+
+        const isFixed = computed(() => container.value === 'body');
+
+        return { modals, getRenderedContent, wrapperId, isFixed };
     },
 });
 </script>
+
+<style lang="scss" src="./VsModalView.scss" />
