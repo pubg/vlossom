431a84d37635d0bf5d4ecc52e97c1876a7e56e41
smithoo <smithoo@pubg.com>
2025-02-07 18:08:33 +0900

    apply readonly on store

--- packages/vlossom/src/stores/option-store.ts
+++ packages/vlossom/src/stores/option-store.ts
@@ -1,4 +1,4 @@
-import { Ref, ref } from 'vue';
+import { readonly, Ref, ref } from 'vue';
 import { VsComponent } from '@/declaration';
 import { utils } from '@/utils';
 
@@ -12,27 +12,28 @@ interface OptionStoreState {
 }
 
 export class OptionStore {
-    public options: Ref<OptionStoreState> = ref({
+    private _options: Ref<OptionStoreState> = ref({
         theme: 'light',
         globalColorScheme: {},
         styleSets: {},
         globalRadiusRatio: 1,
     });
+    public options = readonly(this._options);
 
     getOptions() {
-        return this.options.value;
+        return this._options.value;
     }
 
     setTheme(theme: 'light' | 'dark') {
-        this.options.value.theme = theme;
+        this._options.value.theme = theme;
     }
 
     setGlobalColorScheme(colorScheme: GlobalColorScheme) {
-        this.options.value.globalColorScheme = colorScheme;
+        this._options.value.globalColorScheme = colorScheme;
     }
 
     getGlobalColorScheme(component: VsComponent | string) {
-        return this.options.value.globalColorScheme[component] || this.options.value.globalColorScheme.default;
+        return this._options.value.globalColorScheme[component] || this._options.value.globalColorScheme.default;
     }
 
     setGlobalRadiusRatio(radiusRatio: number) {
@@ -41,16 +42,16 @@ export class OptionStore {
             return;
         }
 
-        this.options.value.globalRadiusRatio = radiusRatio;
+        this._options.value.globalRadiusRatio = radiusRatio;
         document.documentElement.style.setProperty('--vs-radius-ratio', radiusRatio.toString());
     }
 
     registerStyleSet(styleSet: StyleSet) {
         Object.entries(styleSet).forEach(([key, value]) => {
-            const styleSets = this.options.value.styleSets[key as keyof StyleSet];
+            const styleSets = this._options.value.styleSets[key as keyof StyleSet];
 
             if (!styleSets) {
-                this.options.value.styleSets[key as keyof StyleSet] = value;
+                this._options.value.styleSets[key as keyof StyleSet] = value;
             } else {
                 Object.assign(styleSets, value);
             }
@@ -58,6 +59,6 @@ export class OptionStore {
     }
 
     getStyleSet(component: VsComponent | string, styleSetName: string) {
-        return this.options.value.styleSets[component as keyof StyleSet]?.[styleSetName];
+        return this._options.value.styleSets[component as keyof StyleSet]?.[styleSetName];
     }
 }
--- packages/vlossom/src/stores/overlay-callback-store.ts
+++ packages/vlossom/src/stores/overlay-callback-store.ts
@@ -1,18 +1,19 @@
-import { ref, Ref } from 'vue';
+import { ref, Ref, readonly } from 'vue';
 import { OverlayCallbacks, VS_OVERLAY_CLOSE, VS_OVERLAY_OPEN } from '@/declaration';
 
 export class OverlayCallbackStore {
     // overlay tuple: [id, { [eventName: callback }]
-    public overlays: Ref<[string, Ref<OverlayCallbacks>][]> = ref([]);
+    private _overlays: Ref<[string, Ref<OverlayCallbacks>][]> = ref([]);
+    public overlays = readonly(this._overlays);
 
     constructor() {
         document.addEventListener('keydown', (event: KeyboardEvent) => {
-            if (this.overlays.value.length === 0) {
+            if (this._overlays.value.length === 0) {
                 return;
             }
 
             const keyEventName = `key-${event.key}`;
-            const [lastOverlayId, callbacks] = this.overlays.value[this.overlays.value.length - 1];
+            const [lastOverlayId, callbacks] = this._overlays.value[this._overlays.value.length - 1];
             if (!callbacks.value[keyEventName]) {
                 return;
             }
@@ -25,46 +26,46 @@ export class OverlayCallbackStore {
     }
 
     getLastOverlayId() {
-        return this.overlays.value.length > 0 ? this.overlays.value[this.overlays.value.length - 1][0] : '';
+        return this._overlays.value.length > 0 ? this._overlays.value[this._overlays.value.length - 1][0] : '';
     }
 
     async run<T = void>(id: string, eventName: string, ...args: any[]): Promise<T | void> {
-        const index = this.overlays.value.findIndex(([overlayId]) => overlayId === id);
+        const index = this._overlays.value.findIndex(([overlayId]) => overlayId === id);
         if (index === -1) {
             return;
         }
-        const [, callbacks] = this.overlays.value[index];
+        const [, callbacks] = this._overlays.value[index];
         return await callbacks.value[eventName]?.(...args);
     }
 
     push(id: string, callbacks: Ref<OverlayCallbacks>) {
-        this.overlays.value.push([id, callbacks]);
+        this._overlays.value.push([id, callbacks]);
         this.run(id, VS_OVERLAY_OPEN);
         return this.run(id, 'open');
     }
 
     pop(...args: any[]) {
-        const [targetId] = this.overlays.value[this.overlays.value.length - 1];
+        const [targetId] = this._overlays.value[this._overlays.value.length - 1];
         this.run(targetId, VS_OVERLAY_CLOSE, ...args);
         const result = this.run(targetId, 'close', ...args);
-        this.overlays.value.pop();
+        this._overlays.value.pop();
         return result;
     }
 
     remove(id: string, ...args: any[]) {
-        const index = this.overlays.value.findIndex(([stackId]) => stackId === id);
+        const index = this._overlays.value.findIndex(([stackId]) => stackId === id);
         if (index === -1) {
             return;
         }
-        const [targetId] = this.overlays.value[index];
+        const [targetId] = this._overlays.value[index];
         this.run(targetId, VS_OVERLAY_CLOSE, ...args);
         const result = this.run(targetId, 'close', ...args);
-        this.overlays.value.splice(index, 1);
+        this._overlays.value.splice(index, 1);
         return result;
     }
 
     clear(...args: any[]) {
-        while (this.overlays.value.length > 0) {
+        while (this._overlays.value.length > 0) {
             this.pop(...args);
         }
     }
