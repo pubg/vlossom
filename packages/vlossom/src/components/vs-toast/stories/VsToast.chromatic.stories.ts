211b4dfab5130e3bfeb20c12bd1c992d782b3d5a
TaeyangOh <sunio@pubg.com>
2025-05-14 17:16:02 +0900

    feat: VsToast, 크로마틱 테스트를 위한 컴퍼넌트 리스트업

--- packages/vlossom/src/components/vs-toast/stories/VsToast.chromatic.stories.ts
+++ packages/vlossom/src/components/vs-toast/stories/VsToast.chromatic.stories.ts
@@ -1,30 +1,77 @@
-import VsToast from '@/components/vs-toast/VsToast.vue';
-
-import { getMetaArguments } from '@/storybook';
 import type { Meta, StoryObj } from '@storybook/vue3';
+import { VsToastView, VsToast } from '@/components';
+import { store } from '@/stores';
+import { toastPlugin } from '@/plugins';
 
 const meta: Meta<typeof VsToast> = {
     title: 'Chromatic/Base Components/VsToast',
     component: VsToast,
     render: (args: any) => ({
-        components: { VsToast },
+        components: { VsToast, VsToastView },
         setup() {
+            // VsToastView, container='#toast-view'
+            const contents = [
+                { placement: 'top', align: 'start' },
+                { placement: 'top', align: 'center' },
+                { placement: 'top', align: 'end' },
+                { placement: 'middle', align: 'start' },
+                { placement: 'middle', align: 'center' },
+                { placement: 'middle', align: 'end' },
+                { placement: 'bottom', align: 'start' },
+                { placement: 'bottom', align: 'center' },
+                { placement: 'bottom', align: 'end' },
+            ] as const;
+            contents.forEach(({ placement, align }) => {
+                store.toast.remove(`${placement}-${align}`);
+                store.toast.push({
+                    container: '#toast-view',
+                    id: `${placement}-${align}`,
+                    content: `${placement}-${align}`,
+                    placement,
+                    align,
+                });
+            });
+
+            // toastPlugin, container='#body'
+            toastPlugin.show('toast plugin show', { timeout: 500 });
+            toastPlugin.info('toast plugin info', { timeout: 1000 });
+            toastPlugin.success('toast plugin success', { timeout: 1500 });
+            toastPlugin.warn('toast plugin warn', { timeout: 2000 });
+            toastPlugin.error('toast plugin error', { timeout: 2500 });
+
             return { args };
         },
-        template: '<vs-toast v-bind="args">Toast</vs-toast>',
+
+        template: `
+            <div style="display: flex; flex-direction: column; gap: 5rem;">
+                <div style="display: flex; flex-direction: column; height: 25rem;">
+                    <vs-toast v-bind="{ toast: args.toast }" />
+
+                    <vs-toast v-bind="{ toast: {...args.toast, colorScheme: 'gray', content: 'Gray Toast' } }" />
+
+                    <vs-toast v-bind="{ toast: {...args.toast, primary: true, content: 'Primary Toast' } }" />
+
+                    <vs-toast v-bind="{ toast: {...args.toast, autoClose: true, content: 'auto close toast' } }" />
+
+                    <vs-toast v-bind="{ toast: {...args.toast, content: 'Long Long Long Text Toast Content' } }" />
+                </div>
+
+                <div style="position: relative; height: 20rem; ">
+                    <vs-toast-view container="#toast-view" />
+                </div>
+
+                <vs-toast-view container="#body" />
+            </div>
+        `,
     }),
     args: {
         toast: {
-            id: '1',
+            id: 'toast-1',
             content: 'Toast',
         },
     },
-    argTypes: {
-        toast: {},
-    },
 };
 
-meta.args = getMetaArguments(VsToast.props, meta.args);
 export default meta;
 type Story = StoryObj<typeof VsToast>;
 
