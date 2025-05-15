3ef1d6e5b87a9663b378460161898cf4707666b9
TaeyangOh <sunio@pubg.com>
2025-05-15 22:29:09 +0900

    docs(comment): Auto Close 옵션이 custom named container에서 동작하지 않는 이슈

--- packages/vlossom/src/plugins/toast-plugin/toast-plugin.ts
+++ packages/vlossom/src/plugins/toast-plugin/toast-plugin.ts
@@ -10,6 +10,12 @@ import type { ToastPlugin } from './types';
 import type { VsToastInfo, VsToastOptions } from '@/components/vs-toast/types';
 import { useOverlayDom } from '@/composables';
 
+/**
+ * @bug Auto Close 옵션이 custom named container에서 동작하지 않는 이슈
+ *
+ * 1. toast-plugin 과 VsToastView.vue 의 parse 로직 동기화 필요 - `container.replace('#', '#vs-toast-')`
+ * 2. VsToastView.vue의 hasToast가 만족할 수 없는 상태, toast를 만드려면 container가 필요하지만 container는 toast가 있어야 렌더링 된다
+ */
 function renderToastView(toastInfo: VsToastInfo) {
     const { container = 'body' } = toastInfo;
     const containerElement = document.querySelector(container);
