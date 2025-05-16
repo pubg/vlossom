5e519538295bcaf3a165d681475bff4a290d4ba0
smithoo <smithoo@pubg.com>
2025-05-16 17:25:32 +0900

    fix(scroll-lock): prevent add padding on touch device(browser)

--- packages/vlossom/src/utils/index.ts
+++ packages/vlossom/src/utils/index.ts
@@ -1,4 +1,5 @@
 import { clipboardUtil } from './clipboard';
+import { deviceUtil } from './device';
 import { domUtil } from './dom';
 import { functionUtil } from './function';
 import { logUtil } from './log';
@@ -8,6 +9,7 @@ import { stringUtil } from './string';
 
 export const utils = {
     clipboard: clipboardUtil,
+    device: deviceUtil,
     dom: domUtil,
     log: logUtil,
     function: functionUtil,
