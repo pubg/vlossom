889d1395aada3cedac695578152702fcc3909b89
smithoo <smithoo@pubg.com>
2025-05-21 11:23:42 +0900

    fix(scroll-lock): prevent to add padding on touch device(browser) (#357)

--- packages/vlossom/src/composables/scroll-lock-composable.ts
+++ packages/vlossom/src/composables/scroll-lock-composable.ts
@@ -1,41 +1,75 @@
 import { ref } from 'vue';
 import { SCROLLBAR_WIDTH } from '@/declaration';
+import { utils } from '@/utils';
+
+interface ScrollLockState {
+    overflow: string;
+    paddingRight: string;
+    paddingBottom: string;
+}
 
 export function useScrollLock(element: HTMLElement | null) {
-    const originalOverflow = ref('');
-    const originalPaddingRight = ref('0');
-    const originalPaddingBottom = ref('0');
+    const originalState = ref<ScrollLockState>({
+        overflow: '',
+        paddingRight: '0',
+        paddingBottom: '0',
+    });
+    const isNotTouchDevice = !(utils.dom.isBrowser() && utils.device.isTouchDevice());
+
+    function saveOriginalState() {
+        if (!element) {
+            return;
+        }
+
+        originalState.value = {
+            overflow: element.style.overflow,
+            paddingRight: element.style.paddingRight,
+            paddingBottom: element.style.paddingBottom,
+        };
+    }
+
+    function applyScrollLockStyles() {
+        if (!element) {
+            return;
+        }
+
+        element.style.overflow = 'hidden';
+
+        if (isNotTouchDevice) {
+            if (element.scrollHeight >= element.clientHeight) {
+                element.style.paddingRight = SCROLLBAR_WIDTH;
+            }
+            if (element.scrollWidth >= element.clientWidth) {
+                element.style.paddingBottom = SCROLLBAR_WIDTH;
+            }
+        }
+    }
+
+    function restoreOriginalState() {
+        if (!element) {
+            return;
+        }
+
+        element.style.overflow = originalState.value.overflow;
+        element.style.paddingRight = originalState.value.paddingRight;
+        element.style.paddingBottom = originalState.value.paddingBottom;
+    }
 
     function lock() {
         if (!element) {
             return;
         }
-        setTimeout(() => {
-            originalOverflow.value = element.style.overflow;
-            originalPaddingRight.value = element.style.paddingRight;
-            originalPaddingBottom.value = element.style.paddingBottom;
 
-            if (element.scrollHeight >= element.clientHeight) {
-                element.style.paddingRight = SCROLLBAR_WIDTH;
-            }
-
-            if (element.scrollWidth >= element.clientWidth) {
-                element.style.paddingBottom = SCROLLBAR_WIDTH;
-            }
-
-            element.style.overflow = 'hidden';
-        }, 10);
+        saveOriginalState();
+        requestAnimationFrame(applyScrollLockStyles);
     }
 
     function unlock() {
         if (!element) {
             return;
         }
-        setTimeout(() => {
-            element.style.overflow = originalOverflow.value;
-            element.style.paddingRight = originalPaddingRight.value;
-            element.style.paddingBottom = originalPaddingBottom.value;
-        }, 10);
+
+        requestAnimationFrame(restoreOriginalState);
     }
 
     return {
