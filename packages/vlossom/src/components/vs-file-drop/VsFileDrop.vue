b3545f9984d04cd986f1f31c3d11b44000e8bc6d
Taeyang Oh <sunio@pubg.com>
2025-05-29 11:07:36 +0900

    feat(VsFileDrop): implement file insertion via file dialog (#363)

--- packages/vlossom/src/components/vs-file-drop/VsFileDrop.vue
+++ packages/vlossom/src/components/vs-file-drop/VsFileDrop.vue
@@ -1,18 +1,35 @@
 <template>
     <vs-input-wrapper
         v-show="visible"
-        :id="id"
+        :id="computedId"
         :class="classObj"
         :required="required"
+        :messages="computedMessages"
         @mouseenter.stop="onMouseEnter"
         @mouseleave.stop="onMouseLeave"
     >
         <div :class="['vs-file-drop', colorSchemeClass, classObj]" :style="computedStyleSet">
-            <input ref="fileDropRef" class="vs-file-drop-ref" :id="id" type="file" :name="name" :required="required" />
+            <input
+                ref="fileDropRef"
+                class="vs-file-drop-ref"
+                :id="computedId"
+                type="file"
+                :name="name"
+                :required="required"
+                :accept="accept"
+                :multiple="multiple"
+                @change.stop="updateValue($event)"
+            />
 
             <div class="vs-file-drop-content">
                 <slot>
-                    <div class="vs-file-drop-placeholder">
+                    <div v-if="hasValue" class="vs-file-drop-files">
+                        <vs-chip v-for="file in computedInputValue" :key="file.name" closable no-round>
+                            {{ `${file.name} (${file.size} bytes)` }}
+                        </vs-chip>
+                    </div>
+
+                    <div v-else class="vs-file-drop-placeholder">
                         <vs-icon icon="attachFile" size="40" />
                         <span>Drop files here or click to upload</span>
                     </div>
@@ -24,10 +41,10 @@
 
 <script lang="ts">
 import { computed, defineComponent, PropType, ref, Ref, toRefs } from 'vue';
-import { VsComponent, type ColorScheme } from '@/declaration';
+import { StateMessage, VsComponent, type ColorScheme } from '@/declaration';
 import { getInputProps } from '@/models';
-import { useColorScheme, useStyleSet } from '@/composables';
-import { VsInputWrapper } from '@/components';
+import { useColorScheme, useInput, useStyleSet } from '@/composables';
+import { VsChip, VsInputWrapper } from '@/components';
 import { VsIcon } from '@/icons';
 
 import type { InputValueType, VsFileDropStyleSet } from './types';
@@ -35,17 +52,19 @@ import type { InputValueType, VsFileDropStyleSet } from './types';
 const name = VsComponent.VsFileDrop;
 export default defineComponent({
     name,
-    components: { VsInputWrapper, VsIcon },
+    components: { VsInputWrapper, VsIcon, VsChip },
     props: {
         ...getInputProps<InputValueType>(),
+        accept: { type: String, default: '' },
+        multiple: { type: Boolean, default: true },
         colorScheme: { type: String as PropType<ColorScheme> },
         styleSet: { type: [String, Object] as PropType<string | VsFileDropStyleSet> },
         // v-model
-        modelValue: { type: [Object, Array] as PropType<InputValueType>, default: null },
+        modelValue: { type: [Object, Array] as PropType<InputValueType>, default: [] as InputValueType },
     },
-    emits: ['update:modelValue'],
-    setup(props) {
-        const { colorScheme, styleSet, modelValue, disabled } = toRefs(props);
+    emits: ['update:modelValue', 'update:changed', 'change'],
+    setup(props, context) {
+        const { id, colorScheme, styleSet, modelValue, disabled, multiple, rules } = toRefs(props);
 
         const fileDropRef: Ref<HTMLInputElement | null> = ref(null);
 
@@ -53,13 +72,40 @@ export default defineComponent({
 
         const { computedStyleSet } = useStyleSet<VsFileDropStyleSet>(name, styleSet);
 
-        const inputValue = ref(modelValue.value);
+        const inputValue = ref<InputValueType>(modelValue.value);
 
         const hover = ref(false);
 
+        const messages: Ref<StateMessage[]> = ref([]);
+
+        const computedInputValue = computed<File[]>(() => {
+            if (!inputValue.value) {
+                return [];
+            }
+
+            if (Array.isArray(inputValue.value)) {
+                return inputValue.value;
+            }
+
+            return [inputValue.value];
+        });
+
+        const hasValue = computed(() => {
+            return computedInputValue.value.length > 0;
+        });
+
+        const { computedId, computedDisabled, computedMessages, validate } = useInput(context, {
+            inputValue,
+            modelValue,
+            id,
+            disabled,
+            rules,
+            messages,
+        });
+
         const classObj = computed(() => ({
             'vs-hover': hover.value,
-            'vs-disabled': disabled.value,
+            'vs-disabled': computedDisabled.value,
         }));
 
         function onMouseEnter() {
@@ -78,14 +124,50 @@ export default defineComponent({
             hover.value = false;
         }
 
+        function updateValue(event: Event) {
+            const target = event.target as HTMLInputElement;
+            const targetValue = Array.from(target.files || []);
+
+            if (!targetValue.length) {
+                return; // 'cancel' on dialog
+            }
+
+            if (validateSingleFileUploadRule(targetValue)) {
+                messages.value.push({ state: 'error', text: 'You can only upload one file' });
+                validate();
+                return;
+            }
+
+            if (multiple.value) {
+                inputValue.value = targetValue;
+            } else {
+                inputValue.value = targetValue[0] || null;
+            }
+        }
+
+        function validateSingleFileUploadRule(v: InputValueType): string {
+            if (multiple.value) {
+                return '';
+            }
+            if (Array.isArray(v) && v.length > 1) {
+                return 'You can only upload one file';
+            }
+            return '';
+        }
+
         return {
             fileDropRef,
+            computedId,
+            computedMessages,
+            computedInputValue,
             classObj,
             colorSchemeClass,
             computedStyleSet,
             inputValue,
+            hasValue,
             onMouseEnter,
             onMouseLeave,
+            updateValue,
         };
     },
 });
